3c1db5f0e30c3e91d9a4b746be2c9fe326642dd5
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-25 18:11:11 +0000

    Thu, Jan 25, 2024, 8:11 PM +02:00

--- Django1/GHS_app/core/urls.py
+++ Django1/GHS_app/core/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    pasth('signup', views.signup, name='signup'),
+    path('signup', views.signup, name='signup'),
 ]
 
