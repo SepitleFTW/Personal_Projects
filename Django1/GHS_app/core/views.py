76e372bdca9be28c1fbc3ad5cfb2c612da44c88c
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-25 18:26:20 +0000

    Thu, Jan 25, 2024, 8:26 PM +02:00

--- Django1/GHS_app/core/views.py
+++ Django1/GHS_app/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
@@ -10,12 +11,11 @@ def signup(request):
         username = request.POST['username']
         password = request.POST['password']
         email = request.POST['email']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
 
+
+        
         Profile.objects.create(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
 
         return render(request,'signup.html')
     else:
-
         return render(request,'signup.html')
