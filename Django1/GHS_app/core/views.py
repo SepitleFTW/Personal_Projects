d5bb84d5453bbe1ef245695ea18efd3a0b67cade
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-25 18:57:06 +0000

    Thu, Jan 25, 2024, 8:57 PM +02:00

--- Django1/GHS_app/core/views.py
+++ Django1/GHS_app/core/views.py
@@ -32,7 +32,7 @@ def signup(request):
             user_model = User.objects.get(username=username)
             new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
             new_profile.save()
-            return redirect("singup")
+            return redirect("signup")
 
         return render(request,'signup.html')
     else:
