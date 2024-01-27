4bafd4d9f0048d176da0ba24f55223e992e79387
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-01-27 20:33:28 +0000

    Sat, Jan 27, 2024, 10:33 PM +02:00

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
 import curses
+from curses import wrapper
+import time
+import random
+
+def start_screen(stdscre):
+    stdscre.clear()
+    stdscr.addstr("Welcome to the Speed Type Test!")
+    stdscr.addstr("\nPress any key to continue")
+    stdscr.refresh()
+    stdscr.getkey()
+
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(1, 0. f"WPM: {wpm}")
+
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
