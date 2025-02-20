703c3d026c4136a4d1778b00278bb44630f1d01a
Sepitle Leshilo <136067402+SepitleFTW@users.noreply.github.com>
2025-02-20 22:33:05 +0200

    race_results.json has been added, im going to sleep for real

--- Regatta_Results2.0/results.py
+++ Regatta_Results2.0/results.py
@@ -8,21 +8,24 @@ URL for the regatta results page
 URL = "https://www.regattaresults.co.za/home/2025-results/"
 
 def results_scrape():
+    """
+    Function to scrape the regatta results page and save the results to a json file
+    """
     response = requests.get(URL)
-    soup = BeautifulSoup(respones.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    race_data = {}
+    race_data = {} #dict to store race names as keys and URLS as values
 
-    for link in soup.find.all("a", href=True):
-        race_name  = link.text.strip()
+    for link in soup.find_all("a", href=True): #this loop finds all <a> with href attribute
+        race_name  = link.text.strip() #extract text of the link and remove whitespace
         race_url = link["href"]
-        if race_name:
+        if race_name: #storing race data into a dictionary
             race_data[race_name] = race_url
     
     with open("race_results.json", "w") as f:
         json.dump(race_data, f, indent=4)
 
-if __name__ == "__main __":
+if __name__ == "__main__":
     results_scrape()
 
     
